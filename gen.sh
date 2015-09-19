6fed1a84a62ba0ca774ce7e1d9de468000d31930
Lothar Wieske <lothar.wieske@web.de>
2015-09-19 09:51:26 +0200

    Update towards Automation

--- gen.sh
+++ gen.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-updates=("" "u5" "u11" "u20" "u25" "u31" "u40" "u45" "u51" "u60")
-builds=("132" "5" "12" "26" "17" "13" "26" "14" "16" "27")
+rm -rf jdk* jre* server-jre*
+
+# Full Version Strings
+versions=(
+  1.8.0_11-b12
+  1.8.0_20-b26
+  1.8.0_25-b17
+  1.8.0_31-b13
+  1.8.0_40-b26
+  1.8.0_45-b14
+  1.8.0_51-b16
+  1.8.0_60-b27
+)
+
+updates=(    "" "u5" )
+builds=(  "132" "13" )
+
+length=${#versions[@]}+1
+
+for (( i=2; i<${length}+1; i++ ));
+do
+  updates[$i]="u"${versions[$i-2]:6:2}
+  builds[$i]=${versions[$i-2]:10:2}
+done
 
 length=${#updates[@]}
 
@@ -9,14 +31,15 @@ for (( i=1; i<${length}+1; i++ ));
 do
   u=${updates[$i-1]}
   b=${builds[$i-1]}
+  echo "/ U = " ${u} "/ B = " ${b}
   mkdir jdk-8${u} jre-8${u} server-jre-8${u}
-  cat Dockerfile.jdk        >jdk-8${u}/Dockerfile
-  sed -i '' -e s/UPDATE/${u}/g   jdk-8${u}/Dockerfile
-  sed -i '' -e s/BUILD/${b}/g    jdk-8${u}/Dockerfile
-  cat Dockerfile.jre        >jre-8${u}/Dockerfile
-  sed -i '' -e s/UPDATE/${u}/g   jre-8${u}/Dockerfile
-  sed -i '' -e s/BUILD/${b}/g    jre-8${u}/Dockerfile
-  cat Dockerfile.server-jre >server-jre-8${u}/Dockerfile
-  sed -i '' -e s/UPDATE/${u}/g   server-jre-8${u}/Dockerfile
-  sed -i '' -e s/BUILD/${b}/g    server-jre-8${u}/Dockerfile
+  cat Dockerfile.jdk           >jdk-8${u}/Dockerfile
+  sed -i '' -e s/UPDATE/${u}/g  jdk-8${u}/Dockerfile
+  sed -i '' -e s/BUILD/${b}/g   jdk-8${u}/Dockerfile
+  cat Dockerfile.jre           >jre-8${u}/Dockerfile
+  sed -i '' -e s/UPDATE/${u}/g  jre-8${u}/Dockerfile
+  sed -i '' -e s/BUILD/${b}/g   jre-8${u}/Dockerfile
+  cat Dockerfile.server-jre    >server-jre-8${u}/Dockerfile
+  sed -i '' -e s/UPDATE/${u}/g  server-jre-8${u}/Dockerfile
+  sed -i '' -e s/BUILD/${b}/g   server-jre-8${u}/Dockerfile
 done
